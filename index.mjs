5393047829172f015868e185246b267bfb17f247
ahmedabdo007 <ahmedabdoahmed007@gmail.com>
2023-01-04 16:18:33 +0200

    first commit

---
+++ index.mjs
@@ -0,0 +1,16 @@
+import express from 'express'
+import os from 'os'
+
+const app = express()
+const PORT = 3000
+
+app.get("/", (req,res)=>{
+
+    const message=`Hello World, i am pod ${os.hostname()}`
+    res.send(message) 
+})
+
+app.listen(PORT, ()=>{
+    console.log(`web server is listening at port ${PORT}`)
+    console.log(os.hostname)
+})
